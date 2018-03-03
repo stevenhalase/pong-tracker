2385269fce51a9d45fd44ade2d88f8462e4eeeb8
Steven Halase <steven.halase@gmail.com>
2018-03-03 10:11:43 -0800

    Adjusted server

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // app.use(express.static(__dirname + '/build'));
 
-app.use(express.static(__dirname + '/build'))
+app.use(express.static('./build'))
 
 let PlayerRoutes = require('./routes/PlayerRoutes');
 app.use(apiBase + 'players', PlayerRoutes);
